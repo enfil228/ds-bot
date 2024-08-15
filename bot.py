120f5202ead134858d6a37445a0a715fcd307e02
enfil228 <kolyaqwertyuiop2281@gmail.com>
2024-08-15 15:34:50 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands, tasks
+from datetime import datetime
+import pytz
+
+TOKEN = 'MTI1OTEwODYzNTYxNTY5NDk1OQ.G5BVdl.BR99XXPNv9aVqGlLy1vK2OvwiTJWZSAc79SwjI'
+CHANNEL_ID = '1273376272768303218'  # Укажите ID канала, куда будет отправлено сообщение
+MESSAGE = '<@706547063738990613> пруфыыыыы'
+SCHEDULE_TIME = '20:00'  # Укажите время в формате HH:MM
+
+bot = commands.Bot(command_prefix='!')
+
+@tasks.loop(seconds=60)  # Проверка каждую минуту
+async def send_message():
+    now = datetime.now(pytz.timezone('Europe/Moscow')).strftime('%H:%M')
+    if now == SCHEDULE_TIME:
+        channel = bot.get_channel(int(CHANNEL_ID))
+        await channel.send(MESSAGE)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    send_message.start()  # Запуск задачи
+
+bot.run(TOKEN)
